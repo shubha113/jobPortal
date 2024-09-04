d666181881def06fde764cfcf1643aedae999bbf
shubha113 <85664043+shubha113@users.noreply.github.com>
2024-09-04 15:32:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://jobportal-psi-six.vercel.app",
   credentials: true,
 };
 app.use(cors(corsOptions));
