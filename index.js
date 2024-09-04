be76578fd6fa6c7bb0c89d95fd27f1ce05d80e07
shubha113 <85664043+shubha113@users.noreply.github.com>
2024-09-04 14:20:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,15 +16,23 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 const corsOptions = {
   origin: "http://localhost:5173",
   credentials: true,
 };
 app.use(cors(corsOptions));
 
+// Add a root route to serve the HTML content
+app.get("/", (req, res) => 
+  res.send(
+    `<h1>Site is Working. Click <a href="${process.env.FRONTEND_URL}">here</a> to visit frontend.</h1>`
+  )
+);
+
 const PORT = process.env.PORT || 8000;
 
-//api
+//api routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
