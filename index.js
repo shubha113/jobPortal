926ee51b6b8461e06f4398cf2a2c15d2d247bd4a
shubha113 <85664043+shubha113@users.noreply.github.com>
2024-09-05 12:45:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,12 +12,13 @@ import ErrorMiddleware from "./Middlewares/error.js";
 dotenv.config();
 const app = express();
 
-app.use(cors({
-  origin: "https://jobportal-psi-six.vercel.app",   
-  credentials: true,                 
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"], 
-}));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
 
 // Middlewares
 app.use(express.json());
