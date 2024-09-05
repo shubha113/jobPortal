22981497b3b859a1993482ecb30bc59ba8c11610
shubha113 <85664043+shubha113@users.noreply.github.com>
2024-09-05 11:59:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(cors({origin: `http://localhost:5173/`, credentials: true, methods:["GET", "POST","PUT","DELETE"],}));
+app.use(cors({origin: `http://localhost:5173`, credentials: true, methods:["GET", "POST","PUT","DELETE"],}));
 
 
 // Add a root route to serve the HTML content
